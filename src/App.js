332602c52f8ad98ad0291bbbf31c0c9ed15b12d1
Jakub Pepliński <peplinjak@gmail.com>
2024-01-26 22:03:47 +0100

    UserPage, LoginPage ++

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { DataProvider } from "./context/DataContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserPage from "./pages/UserPage";
+import Login from "./pages/Login";
+import './styles/app.css'
 
 function App() {
-  
-
-
   return (
     <Router>
-    <DataProvider>
-      <Routes>
-
-        <Route path="/" exact element={<Home />} />
-
-      </Routes>
-    </DataProvider>
+      <DataProvider>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/user/:userID" element={<UserPage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </DataProvider>
     </Router>
   );
 }
